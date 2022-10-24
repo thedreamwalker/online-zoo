507a84c9b75765e98caae91519056f3b80a90416
Veronica P <vhayami@gmail.com>
2022-10-24 16:03:48 +0300

    fix: size for enable popup

--- online-zoo/pages/main/js/script.js
+++ online-zoo/pages/main/js/script.js
@@ -241,7 +241,7 @@ testimonialsContainer.addEventListener('click', event => {
       });
   };
 
-  if (event.target.closest('.testimonials__item') && window.innerWidth < 1000) {
+  if (event.target.closest('.testimonials__item') && window.innerWidth < 639) {
     testimonialsPopUpOpen();
     const id = event.target.closest('.testimonials__item').getAttribute('data-id');
 
